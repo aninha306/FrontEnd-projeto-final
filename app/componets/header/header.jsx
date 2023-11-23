5c7a8ad323febcaa8f2e40adf93c918d598bc097
aninha306 <ana-pontes@aluno.senai.br>
2023-11-23 16:58:34 -0300

    first commit

---
+++ app/componets/header/header.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import styles from './header.module.css';
+
+function Header() {
+    return (
+        <div className={styles.header}>
+
+            <div className={styles.img}>
+                <Image src={"/logoArteAlémdaTela.jpeg"} width={150} height={150} className={styles.imagem}/>
+            </div>
+
+            <div>
+                <Link className={styles.links} href={'/'}>Home</Link>
+                <Link className={styles.links} href={'/'}>Sobre nós</Link>
+
+                <Link className={styles.links} href={'/'}>Contato</Link>
+            </div>
+
+            <div className={styles.cadastro}>
+                <button className={styles.butaocadastro}>Cadastro</button>
+            </div>
+
+        </div>
+    )
+}
+export default Header;
