47a8f6c14764b10275049d1751bdb6c435c840b7
Luana Fassini <luanafassini.564@gmail.com>
2023-11-28 14:43:52 -0300

    feat: create contato.module.css

---
+++ app/componets/contato/Contato.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import styles from './header.module.css';
+
+function Contato() {
+    return (
+
+        
+        <div className={styles.contato}>
+
+    <Header></Header>
+
+            <div>
+                
+                <input className={styles.nome}  href={'/'} placeholder="Nome">Nome</input>
+                <input className={styles.email} href={'/'} placeholder="Email"><Email></Email></input>
+
+            </div>
+
+            <div className={styles.contato}>
+                <button className={styles.enviarMsg}>Enviar</button>
+            </div>
+
+            <Footer></Footer>
+
+        </div>
+    )
+}
+export default Contato;
