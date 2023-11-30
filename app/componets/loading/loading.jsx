d13bb8c5bd7c71f41df1cbf6d94fcde411fff78c
Luana Fassini <luanafassini.564@gmail.com>
2023-11-30 14:37:53 -0300

    feat: init contato page.jsx

--- app/componets/loading/loading.jsx
+++ app/componets/loading/loading.jsx
@@ -9,4 +9,4 @@ function Loading() {
     )
 }
 
-export default ImageLoading
+export default ImageLoading;
---
+++ app/contato/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function contato() {
+  return (
+    <div>Contato</div>
+  )
+}
+
+export default contato;
