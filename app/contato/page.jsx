15dc200f300d6a97489a2a8369081e8eec3b8888
Luana Fassini <luanafassini.564@gmail.com>
2023-11-30 14:46:57 -0300

    feat: init contato page.jsx1

--- app/contato/page.jsx
+++ app/contato/page.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import styles from "./contato.module.css"
+import { text } from 'express';
 
 function contato() {
   return (
-    <div>Contato</div>
+    <div className={styles.principal}>
+        <div className={styles.titulo}>
+            <h1> Contato </h1>
+        </div>
+
+        <div className={styles.imagem}>
+            <img src="/contato.png" alt="Imagem de contato" />
+        </div>
+
+        <div className={styles.divinputs}>
+            <input 
+            className={styles.inp}
+            value={state}
+            type={text}
+            name={nome}
+            placeholder={texto}
+            onChange={(e) => setState(e.target.value)}
+    />
+            
+        </div>
+
+        <div className={styles.divicones}>
+           
+        </div>
+    </div>
   )
 }
 
