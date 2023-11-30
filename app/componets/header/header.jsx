a4a255407e9c7f73848d0d32410e065ea83618fb
Luana Fassini <luanafassini.564@gmail.com>
2023-11-30 09:26:30 -0300

    feat: comentarios header.jsx

--- app/componets/header/header.jsx
+++ app/componets/header/header.jsx
@@ -3,14 +3,20 @@ import Link from "next/link";
 import Image from "next/image";
 import styles from './header.module.css';
 
+//funcao responsavel pelo header//
+
 function Header() {
     return (
+        //div style.header responsavel por armazenar tudo
+        //div style.img responsavel pela imagem
+        //div style.rotas responsavel pelas rotas de home, sobre nos, detalhes e contato
+        // div style.cadastro responsavel por armazenar o botao de cadastrar
         <div className={styles.header}>
 
             <div className={styles.img}>
                 <Image src={"/logoArteAlémdaTela.jpeg"} width={150} height={150} className={styles.imagem}/>
             </div>
-
+      
             <div className={styles.rotas}>
                 <Link className={styles.links} href={'/'}>Home</Link>
                 <Link className={styles.links} href={'/'}>Sobre nós</Link>
@@ -26,3 +32,4 @@ function Header() {
     )
 }
 export default Header;
+//exportar funçao
