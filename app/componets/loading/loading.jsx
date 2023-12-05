2cae32f154cc3c6a9e95d7173808e852618d98d5
Luana Fassini <luanafassini.564@gmail.com>
2023-12-05 09:23:12 -0300

    feat: create loading3

--- app/componets/loading/loading.jsx
+++ app/componets/loading/loading.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import styles from './imageLoading.module.css'
+import Header from '../header/header';
+import Footer from '../footer/footer';
 
 function ImageLoading() {
+
+    <Header/>
     return (
         <div id={styles.containerImgLoading}>
                 <img id={styles.image} src='/loading.gif' alt="Loading Image" />
         </div>
     )
 }
+
+<Footer/>
 
 export default ImageLoading;
