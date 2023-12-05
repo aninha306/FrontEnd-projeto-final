89303c6d1e9ca3f59caad8b7861891fcd42968d5
Luana Fassini <luanafassini.564@gmail.com>
2023-12-05 08:31:28 -0300

    feat: create popup jsx new

--- app/componets/popup/PopUp.jsx
+++ app/componets/popup/PopUp.jsx
@@ -1,14 +1,42 @@
-import React from 'react';
-import styles from './popup.module.css';
+"use client"
+import React, { useState } from 'react';
+import PopUp from '../components/popUp/PopUp';
+import Header from '../components/header/Header';
 
-const Popup = ({icon1, message, icon2, type}) => { 
-    var color = (type === 'success') ? styles.success : styles.error;
-    var mainDiv = styles.mainDiv + ' ' + color;
+const App = () => {
+  const [showPopup, setShowPopup] = useState(false);
+  const [popupMessage, setPopupMessage] = useState('');
+  const [popupType, setPopupType] = useState('');
+
+  function handleShowPopup(message, type, time) {
+    setPopupMessage(message)
+    setPopupType(type)
+    setShowPopup(true)
+    setTimeout(() => {
+      setShowPopup(false)
+    }, time)
+  }
+
   return (
-    <div className={mainDiv}>
-        <p>{icon1}<span id={styles.message}>{message}</span>{icon2}</p> 
+    <div>
+      <Header />
+
+      <button onClick={() => handleShowPopup('Erro ao processar!', 'error', 4000)}>
+        Exibir mensagem de erro
+      </button>
+      
+      <button onClick={() => handleShowPopup('Mensagem Enviada!', 'success', 6000)}>
+        Exibir mensagem de sucesso
+      </button>
+
+      {showPopup && (
+        <PopUp
+          message={popupMessage}
+          type={popupType}
+        />
+      )}
     </div>
   );
 };
 
-export default Popup;
+export default App;
