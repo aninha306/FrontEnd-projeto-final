3fa9033c2508738aad93ce135db53c21040a6cae
Luana Fassini <luanafassini.564@gmail.com>
2023-12-05 14:59:28 -0300

    feat: coment code1

--- app/cadastro2/[id]/page.jsx
+++ app/cadastro2/[id]/page.jsx
@@ -3,11 +3,14 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import { useRouter } from "next/navigation";
 
-import style from "./id.module.css";
+import style from "./id.module.css"; 
 import Header from "@/app/componets/header/header";
 import Footer from "@/app/componets/footer/footer";
 import Link from "next/link";
 import e from "express";
+
+// esse componente React parece estar configurado para atualizar os dados de uma obra de arte, utilizando os 
+//estados para armazenar informações e o roteador para direcionar o usuário após a conclusão da atualização.
 
 export default function AtualizarCadastro({ params }) {
     const [nomeObra, setNome] = useState("");
@@ -18,7 +21,8 @@ export default function AtualizarCadastro({ params }) {
     const [idadeArtista, setIdade] = useState("");
     const { id } = params;
     const router = useRouter();
-
+//este trecho de código busca informações específicas de uma obra de arte na API utilizando o ID fornecido, 
+//e preenche os campos do formulário de cadastro com esses detalhes para permitir a edição ou atualização dos dados.
     useEffect(() => {
         async function BuscarObra() {
             try {
@@ -34,13 +38,17 @@ export default function AtualizarCadastro({ params }) {
                 console.error("ERROR BUSCAR OBRA:", error);
             }
         }
-        if (id) {
+        //esse código trata da atualização dos dados da obra de arte na API quando o formulário é submetido, 
+        //utilizando o método PUT da API para enviar os novos dados e redirecionar o usuário após a conclusão da atualização.
+     if (id) {
             BuscarObra();
         }
     }, [id]);
     const handleSubmit = async (e) => {
         e.prevenirObra();
-
+//este trecho de código trata da requisição de atualização dos dados da obra de arte na API e do redirecionamento
+//do usuário para a página de cadastro após a operação ser concluída com êxito. Em caso de falha, exibe 
+//informações do erro no console para fins de depuração.
         try {
             await axios.put(`/API/cadastro/${id}`, { nomeObra, url, artista, dataProducao, tipo, idadeArtista });
             router.push(`/cadastro/`);
@@ -48,6 +56,9 @@ export default function AtualizarCadastro({ params }) {
             console.error("Error atualizar cadastro", error);
         }
     };
+//Este trecho de código representa uma estrutura de interface de usuário em React. Ele cria contêineres e um 
+//botão estilizado que redireciona o usuário para a página de cadastro quando clicado. As classes de estilo 
+//definem a aparência desses elementos na interface.
     return (
         <div className={style.container}>
          <div className={style.acoes}> 
@@ -144,5 +155,7 @@ export default function AtualizarCadastro({ params }) {
         </div>
     );
 }
+//Esse código possibilita a atualização das informações de uma obra por meio de um formulário interativo na 
+//interface do usuário.
 
 
