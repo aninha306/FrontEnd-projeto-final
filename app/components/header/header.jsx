6532e849f0f2b54d108f2b624e1693cae3ea2b56
Luana Fassini <luanafassini.564@gmail.com>
2023-12-07 17:23:39 -0300

    feat: create responsive home1

--- app/components/header/header.jsx
+++ app/components/header/header.jsx
@@ -14,12 +14,14 @@ function Header() {
             <div className={styles.rotas}>
                 <Link className={styles.links} href={'/'}>Home</Link>
                 <Link className={styles.links} href={'/'}>Sobre nós</Link>
-                <Link className={styles.links} href={'/'}>Quadros</Link>
+                <Link className={styles.links} href={'/cadastro2'}>Quadros</Link>
                 <Link className={styles.links} href={'/contato'}>Contato</Link>
             </div>
 
             <div className={styles.cadastro}>
-                <button className={styles.butaocadastro}>Cadastro</button>
+                <Link className={styles.link1} href={'/cadastro2/register'}>
+                    <button className={styles.butaocadastro}>Cadastro</button>
+                </Link>
             </div>
 
         </div>
