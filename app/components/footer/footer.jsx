d9773b457c8db1d55c7a8bedfd9ee77ab1bc5d78
aninha306 <ana-pontes@aluno.senai.br>
2023-12-12 08:44:05 -0300

    footer

--- app/components/footer/footer.jsx
+++ app/components/footer/footer.jsx
@@ -19,7 +19,7 @@ function Footer(){
                 <div className={styles.camihos}>
                 <Link className={styles.links} href={"/"}>HOME</Link>
                     <Link className={styles.links} href={"/"}>SOBRE NÓS</Link>
-                    <Link className={styles.links} href={"/cadastro2"}>QUADROS</Link>
+                    <Link className={styles.links} href={"/cadastro2"}>COLABORADORES</Link>
                     <Link className={styles.links} href={"/contato"}>CONTATO</Link>
                     <Link className={styles.links} href={"/cadastro2/register"}>CADASTRO</Link>
                 </div>
