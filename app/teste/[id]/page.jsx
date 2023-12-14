c24f2d481c051bfbc53c19c4dd88c6c00a449954
IsaBarquilia <isabelle.barquilia.564@gmail.com>
2023-12-14 14:05:56 -0300

    termino projeto

--- app/teste/[id]/page.jsx
+++ app/teste/[id]/page.jsx
@@ -71,73 +71,40 @@ export default function Editar({ params }) {
   return (
     <>
     <Header/>
-      <div className={styles.container}>
+    
+        <div>
         <div className={styles.arteContainer}>
           <h1 className={styles.mainText}>Atualizar Arte</h1>
           {id ? (
             <form onSubmit={handleSubmit}>
               <div className={styles.formGroup}>
-                <label htmlFor="url" className={styles.label}>
-                  URL:
-                </label>
-                <input
-                  type="text"
-                  value={url}
-                  onChange={(e) => setUrl(e.target.value)}
-                  required
-                />
-              </div>
+
               <div className={styles.formGroup}>
-                <label htmlFor="nomeObra" className={styles.label}>
-                  Nome:
-                </label>
+            <p>Nome da sua obra:</p>
                 <input
                   type="text"
+                  className={styles.input}
                   value={nomeObra}
                   onChange={(e) => setNomeObra(e.target.value)}
                   required
                 />
               </div>
+             <p>URL da sua obra:</p>
+                <input
+                  type="text"
+                  className={styles.input}
+                  value={url}
+                  onChange={(e) => setUrl(e.target.value)}
+                  required
+                />
+              </div>
+           
 
               <div className={styles.formGroup}>
-                <label htmlFor="tipo" className={styles.label}>
-                  Tipo da Obra:
-                </label>
+              <p>Nome do artista:</p>
                 <input
                   type="text"
-                  value={tipo}
-                  onChange={(e) => setTipo(e.target.value)}
-                  required
-                />
-              </div>
-              <div className={styles.formGroup}>
-                <label htmlFor="idadeArtista" className={styles.label}>
-                  Idade do Artista:
-                </label>
-                <input
-                  type="text"
-                  value={idadeArtista}
-                  onChange={(e) => setIdadeArtista(e.target.value)}
-                  required
-                />
-              </div>
-              <div className={styles.formGroup}>
-                <label htmlFor="dataProducao" className={styles.label}>
-                  Data de Produção:
-                </label>
-                <input
-                  type="text"
-                  value={dataProducao}
-                  onChange={(e) => setDataProducao(e.target.value)}
-                  required
-                />
-              </div>
-              <div className={styles.formGroup}>
-                <label htmlFor="artista" className={styles.label}>
-                  Artista:
-                </label>
-                <input
-                  type="text"
+                  className={styles.input}
                   value={artista}
                   onChange={(e) => setArtista(e.target.value)}
                   required
@@ -145,16 +112,39 @@ export default function Editar({ params }) {
               </div>
 
               <div className={styles.formGroup}>
-                <label htmlFor="descricao" className={styles.label}>
-                  Descrição:
-                </label>
+              <p>Tipo da obra</p>
                 <input
                   type="text"
-                  value={descricao}
-                  onChange={(e) => setDescricao(e.target.value)}
+                  className={styles.input}
+                  value={tipo}
+                  onChange={(e) => setTipo(e.target.value)}
                   required
                 />
               </div>
+
+              <div className={styles.formGroup}>
+                <p>Data producao:</p>
+                <input
+                  type="text"
+                  className={styles.input}
+                  value={dataProducao}
+                  onChange={(e) => setDataProducao(e.target.value)}
+                  required
+                />
+              </div>
+
+              <div className={styles.formGroup}>
+               <p>Idade do artista:</p>
+                <input
+                  type="text"
+                  className={styles.input}
+                  value={idadeArtista}
+                  onChange={(e) => setIdadeArtista(e.target.value)}
+                  required
+                />
+              </div>
+           
+    
               <button
                 type="submit"
                 className={`${styles.button} ${styles.submitButton}`}
@@ -162,12 +152,7 @@ export default function Editar({ params }) {
               >
                 Salvar alterações
               </button>
-              {/* <button
-                type="submit"
-                onClick={() => (router.push= '/')}
-              >
-                voltar para obras
-              </button> */}
+          
             </form>
           ) : (
             <p>Carregando...</p>
